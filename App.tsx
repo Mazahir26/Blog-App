aa080e0e2933f2b0b51e4d9d5931706ba4231590
Mazahir26 <supermazahir@gmail.com>
2021-09-29 18:25:22 +0530

    Added Appcontrol and AuthFlow

--- App.tsx
+++ App.tsx
@@ -1,21 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import AppControl from "./src/screens/Appcontrol";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <>
       <StatusBar style="auto" />
-    </View>
+      <AppControl />
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
