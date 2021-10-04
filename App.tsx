dbcf158ccf3c75e8af813c1ea552e7d54613fe39
Mazahir26 <supermazahir@gmail.com>
2021-10-04 20:38:17 +0530

    Login screen

--- App.tsx
+++ App.tsx
@@ -1,9 +1,6 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import AppControl from "./src/screens/Appcontrol";
-import { firebase } from "@firebase/app";
-import { firebaseConfig } from "./src/config";
-firebase.initializeApp(firebaseConfig);
 export default function App() {
   return (
     <>
--- src/screens/Auth.tsx
+++ src/screens/Auth.tsx
@@ -1,69 +1,35 @@
 import * as React from "react";
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
-import {
-  createMaterialTopTabNavigator,
-  MaterialTopTabScreenProps,
-} from "@react-navigation/material-top-tabs";
+import { View, Text, StyleSheet } from "react-native";
+import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
+import Login from "./Login";
 import MyTabBar from "../components/TabBar";
 const Tab = createMaterialTopTabNavigator();
-import BottomSheet from "@gorhom/bottom-sheet";
-type RootStackParamList = {
-  Login: {} | undefined;
-  SignUp: {} | undefined;
-};
-
-type Props = MaterialTopTabScreenProps<RootStackParamList>;
-
-function Login({ navigation }: Props) {
-  console.log(navigation);
-  return (
-    <View
-      style={{
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
-      }}
-    >
-      <Text>Login</Text>
-    </View>
-  );
-}
+// type RootStackParamList = {
+//   Login: {} | undefined;
+//   SignUp: {} | undefined;
+// };
+//
+// type Props = MaterialTopTabScreenProps<RootStackParamList>;
+//
+// function Login({ navigation }: Props) {
+//   return (
+//     <View
+//       style={{
+//         flex: 1,
+//         alignItems: "center",
+//         justifyContent: "center",
+//       }}
+//     >
+//       <TouchableOpacity onPress={() => navigation.navigate("SignUp")}>
+//         <Text>Login</Text>
+//       </TouchableOpacity>
+//     </View>
+//   );
+// }
 function SignUp() {
-  const bottomSheetRef = React.useRef<BottomSheet>(null);
-
-  // variables
-  const snapPoints = React.useMemo(() => ["25%", "50%"], []);
-
-  // callbacks
-  const handleSheetChanges = React.useCallback((index: number) => {
-    console.log("handleSheetChanges", index);
-  }, []);
-  const handleClosePress = React.useCallback(() => {
-    bottomSheetRef.current?.close();
-  }, []);
-  const handleOpenPress = React.useCallback(() => {
-    bottomSheetRef.current?.collapse();
-  }, []);
-  // renders
   return (
     <View style={styles.container}>
-      <BottomSheet
-        ref={bottomSheetRef}
-        index={-1}
-        snapPoints={snapPoints}
-        onChange={handleSheetChanges}
-        enablePanDownToClose={true}
-      >
-        <View style={styles.contentContainer}>
-          <Text>Awesome 🎉</Text>
-        </View>
-      </BottomSheet>
-      <TouchableOpacity onPress={handleClosePress}>
-        <Text>SingUp</Text>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={handleOpenPress}>
-        <Text>open</Text>
-      </TouchableOpacity>
+      <Text>SingUp</Text>
     </View>
   );
 }
@@ -80,10 +46,7 @@ export default function MyTabs() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 24,
-  },
-  contentContainer: {
-    flex: 1,
     alignItems: "center",
+    justifyContent: "center",
   },
 });
