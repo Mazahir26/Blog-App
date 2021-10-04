da63f258a7152566b62a81541a37de202c00e3b8
Mazahir26 <supermazahir@gmail.com>
2021-10-04 16:53:10 +0530

    Added firebase config & bottom sheet

--- App.tsx
+++ App.tsx
@@ -1,7 +1,9 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import AppControl from "./src/screens/Appcontrol";
-
+import { firebase } from "@firebase/app";
+import { firebaseConfig } from "./src/config";
+firebase.initializeApp(firebaseConfig);
 export default function App() {
   return (
     <>
--- src/screens/Auth.tsx
+++ src/screens/Auth.tsx
@@ -1,15 +1,21 @@
 import * as React from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 import {
-  View,
-  Text,
-  TouchableOpacity,
-  Animated,
-  StyleSheet,
-} from "react-native";
-import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
+  createMaterialTopTabNavigator,
+  MaterialTopTabScreenProps,
+} from "@react-navigation/material-top-tabs";
+import MyTabBar from "../components/TabBar";
 const Tab = createMaterialTopTabNavigator();
+import BottomSheet from "@gorhom/bottom-sheet";
+type RootStackParamList = {
+  Login: {} | undefined;
+  SignUp: {} | undefined;
+};
 
-function Login() {
+type Props = MaterialTopTabScreenProps<RootStackParamList>;
+
+function Login({ navigation }: Props) {
+  console.log(navigation);
   return (
     <View
       style={{
@@ -23,9 +29,41 @@ function Login() {
   );
 }
 function SignUp() {
+  const bottomSheetRef = React.useRef<BottomSheet>(null);
+
+  // variables
+  const snapPoints = React.useMemo(() => ["25%", "50%"], []);
+
+  // callbacks
+  const handleSheetChanges = React.useCallback((index: number) => {
+    console.log("handleSheetChanges", index);
+  }, []);
+  const handleClosePress = React.useCallback(() => {
+    bottomSheetRef.current?.close();
+  }, []);
+  const handleOpenPress = React.useCallback(() => {
+    bottomSheetRef.current?.collapse();
+  }, []);
+  // renders
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>SingUp</Text>
+    <View style={styles.container}>
+      <BottomSheet
+        ref={bottomSheetRef}
+        index={-1}
+        snapPoints={snapPoints}
+        onChange={handleSheetChanges}
+        enablePanDownToClose={true}
+      >
+        <View style={styles.contentContainer}>
+          <Text>Awesome 🎉</Text>
+        </View>
+      </BottomSheet>
+      <TouchableOpacity onPress={handleClosePress}>
+        <Text>SingUp</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={handleOpenPress}>
+        <Text>open</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -33,134 +71,19 @@ function SignUp() {
 export default function MyTabs() {
   return (
     <Tab.Navigator tabBar={(props: any) => <MyTabBar {...props} />}>
-      <Tab.Screen name="Home" component={Login} />
-      <Tab.Screen name="Settings" component={SignUp} />
+      <Tab.Screen name="Login" component={Login} />
+      <Tab.Screen name="SignUp" component={SignUp} />
     </Tab.Navigator>
   );
 }
 
-function MyTabBar({ state, descriptors, navigation, position }: any) {
-  const width = 100;
-  const inputRange = state.routes.map((_: any, i: any) => i);
-  const op = position.interpolate({
-    inputRange,
-    outputRange: inputRange.map((i: any) => i * (width + 5)),
-  });
-  return (
-    <View style={styles.TabBar}>
-      <Animated.View
-        style={{
-          position: "absolute",
-          height: 45,
-          width: width,
-          backgroundColor: "#343434",
-          transform: [{ translateX: op }],
-          borderRadius: width / 2,
-        }}
-      />
-      <View
-        style={{
-          flexDirection: "row",
-          borderWidth: 1,
-          width: state.routes.length * width + 5,
-          height: 45,
-          borderRadius: (state.routes.length * width + 5) / 2,
-        }}
-      >
-        {state.routes.map((route: any, index: any) => {
-          const { options } = descriptors[route.key];
-          const label =
-            options.tabBarLabel !== undefined
-              ? options.tabBarLabel
-              : options.title !== undefined
-              ? options.title
-              : route.name;
-
-          const isFocused = state.index === index;
-
-          const onPress = () => {
-            const event = navigation.emit({
-              type: "tabPress",
-              target: route.key,
-              canPreventDefault: true,
-            });
-
-            if (!isFocused && !event.defaultPrevented) {
-              navigation.navigate({ name: route.name, merge: true });
-            }
-          };
-
-          const onLongPress = () => {
-            navigation.emit({
-              type: "tabLongPress",
-              target: route.key,
-            });
-          };
-
-          const inputRange = state.routes.map((_: any, i: any) => i);
-          const opacity = position.interpolate({
-            inputRange,
-            outputRange: inputRange.map((i: any) => (i === index ? 1 : 0)),
-          });
-          const opacity_2 = position.interpolate({
-            inputRange,
-            outputRange: inputRange.map((i: any) => (i === index ? 0 : 1)),
-          });
-          return (
-            <TouchableOpacity
-              key={label}
-              testID={options.tabBarTestID}
-              onPress={onPress}
-              onLongPress={onLongPress}
-              style={{
-                width: width,
-                marginRight: 5,
-              }}
-            >
-              <Animated.View
-                style={{
-                  justifyContent: "center",
-                  flex: 1,
-                  borderRadius: width / 2,
-                  alignItems: "center",
-                }}
-              >
-                <Animated.Text
-                  style={{
-                    opacity,
-                    textAlign: "center",
-                    color: "white",
-                    position: "absolute",
-                  }}
-                >
-                  {label}
-                </Animated.Text>
-                <Animated.Text
-                  style={{
-                    opacity: opacity_2,
-                    textAlign: "center",
-                    color: "black",
-                    position: "absolute",
-                  }}
-                >
-                  {label}
-                </Animated.Text>
-              </Animated.View>
-            </TouchableOpacity>
-          );
-        })}
-      </View>
-    </View>
-  );
-}
-
 const styles = StyleSheet.create({
-  TabBar: {
-    marginTop: 30,
-    alignSelf: "center",
+  container: {
+    flex: 1,
+    padding: 24,
+  },
+  contentContainer: {
+    flex: 1,
+    alignItems: "center",
   },
 });
-
-// accessibilityRole="button"
-// accessibilityState={isFocused ? { selected: true } : {}}
-// accessibilityLabel={options.tabBarAccessibilityLabel}
