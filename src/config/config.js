d6f05fffbe0682813b2e6b9b6a5fd327926e2077
Mazahir26 <supermazahir@gmail.com>
2021-10-14 18:26:58 +0530

    added config template

---
+++ src/config/config.js
@@ -0,0 +1,13 @@
+export const firebaseConfig = {
+  apiKey: "Firebase Api key",
+  authDomain: "authDomain",
+  projectId: "projectId",
+  storageBucket: "storageBucket",
+  messagingSenderId: "messagingSenderId",
+  appId: "appId",
+  measurementId: "measurementId",
+};
+export const config = {
+  baseURL: "https://example.herokuapp.com/",
+  api_key: "Token",
+};
